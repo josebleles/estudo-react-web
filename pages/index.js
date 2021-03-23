323b546a0ae8c9272adc570e51aad3ba6ae7842f
José Leles <jose.leles@fiap.com.br>
2021-03-23 11:34:18 -0300

    feat: studyng react with three

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import { React } from 'react';
+
+
+function Home(props){
+
+    return <div>
+        Sumário :
+        <ul>
+            <li><a href="react-three">React with three.js</a>, references <a href="https://threejs.org/">threejs.org</a></li>
+        </ul>
+    </div>;
+}
+
+export default Home;
